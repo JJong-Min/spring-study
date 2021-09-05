4bcba973c5f3f24b3b7ff17948a29a822e1ddb88
JJong-Min <sambujakkk@naver.com>
2021-09-05 09:32:17 +0900

    controller 코드입니다.

--- hello-spring/src/main/java/hello/hellospring/controller/hellocontroller.java
+++ hello-spring/src/main/java/hello/hellospring/controller/hellocontroller.java
@@ -3,18 +3,55 @@ package hello.hellospring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-public class hellocontroller {
-    //get방식으로 넘어온 /hello에 매핑시켜주는 것임.
+public class HelloController {
+    // Web Application에서 '/hello'로 들어오면 해당 메소드를 호출
     @GetMapping("hello")
-    //요청이 오면 아래 메서드가 실행됨.
-    //spring이 model을 만들어서 키:값을 넣음.
-    public String Hello(Model model) {
+    public String hello(Model model) {
         model.addAttribute("data", "hello!!");
-        // resources/templates에 있는 hello.html에 렌더링하라는 의미, model을 가지고 가면서
+        // 스프링 부트는 기본적으로 resouces\templates 내의 hello.html을 찾아서 렌더링함.
         return "hello";
 
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam(value = "name") String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello-template";
+
+    }
+
+    @GetMapping("hello-spring")
+    // response body부에 해당 내용을 직접 넣어주겠다는 의미
+    @ResponseBody
+    public String helloString(@RequestParam("name") String name) {
+        return "hello" + name;
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name) {
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+
+    static class Hello {
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
+
+    public static void main(String[] args) {
 
     }
 }
