4816122096918d6519521becb4798d7e62014ffd
JJong-Min <sambujakkk@naver.com>
2021-09-05 09:33:37 +0900

    spring실행 코드입니다.

--- hello-spring/src/main/java/hello/hellospring/HelloSpringApplication.java
+++ hello-spring/src/main/java/hello/hellospring/HelloSpringApplication.java
@@ -3,7 +3,8 @@ package hello.hellospring;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-//spring을 web server에서 실행되도록 해주는 class
+// spring을 web server에서 실행되도록 해주는 class
+// Tomcat이라는 웹 서버를 내장하고 있음
 @SpringBootApplication
 public class HelloSpringApplication {
 
